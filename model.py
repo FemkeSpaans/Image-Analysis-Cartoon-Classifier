8605c714774061005229653c68e5ec1a272ec5d8
evanijhuis <EAH.Nijhuis@student.han.nl>
2023-05-23 10:12:52 +0200

    script aanpassingen model bouwen werkt zover

--- model.py
+++ model.py
@@ -105,8 +105,7 @@ def model_voor_augmentatie(train, validatie, class_names):
     ])
 
     model_voor_augmentatie.compile(optimizer='adam',
-                                   loss=tf.keras.losses.SparseCategoricalCrossentropy(
-                                       from_logits=True),
+                                   loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
                                    metrics=['accuracy'])
     model_voor_augmentatie.summary()
 
@@ -116,7 +115,7 @@ def model_voor_augmentatie(train, validatie, class_names):
         epochs=epochs
     )
 
-    data_augmentation = keras.Sequential(
+    data_aug = keras.Sequential(
         [
             layers.RandomFlip("horizontal",
                               input_shape=(img_height, img_width, 3)),
@@ -135,7 +134,7 @@ def model_voor_augmentatie(train, validatie, class_names):
                                 r"Image-Analysis-Cartoon-Classifier\\"
                                 r"model_voor_augmentatie")
 
-    return t_acc, v_acc, t_loss, v_loss, epochs_range, data_augmentation
+    return t_acc, v_acc, t_loss, v_loss, epochs_range, data_aug
 
 
 def figure_model(t_acc, v_acc, t_loss, v_loss, epochs_range):
@@ -167,10 +166,102 @@ def figure_model(t_acc, v_acc, t_loss, v_loss, epochs_range):
     plt.show()
 
 
+def augmentation_figure(trainset, data_aug):
+    """
+
+    :param trainset:
+    :param data_aug:
+    :return:
+    """
+    plt.figure(figsize=(10, 10))
+    for images, _ in trainset.take(1):
+        for i in range(9):
+            augmented_images = data_aug(images)
+            ax = plt.subplot(3, 3, i + 1)
+            plt.imshow(augmented_images[0].numpy().astype("uint8"))
+            plt.axis("off")
+
+
+def model_aug(data_aug, class_names, trainset, validatieset):
+    """
+
+    :return:
+    """
+    num_classes = len(class_names)
+    model = Sequential([
+        data_aug,
+        layers.Rescaling(1. / 255),
+        layers.Conv2D(16, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Conv2D(32, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Conv2D(64, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Dropout(0.2),
+        layers.Flatten(),
+        layers.Dense(128, activation='relu'),
+        layers.Dense(num_classes, name="outputs")
+    ])
+
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(
+                      from_logits=True),
+                  metrics=['accuracy'])
+
+    model.summary()
+
+    epochs = 15
+    history = model.fit(
+        trainset,
+        validation_data=validatieset,
+        epochs=epochs
+    )
+
+    model.save(r"C:\Users\eahni\Image-Analysis-Cartoon-Classifier\model_na_augmentatie")
+
+    return history, epochs
+
+
+def figure_aug(history, epochs):
+    """
+
+    :param history:
+    :return:
+    """
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs_range = range(epochs)
+
+    plt.figure(figsize=(8, 8))
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs_range, acc, label='Training Accuracy')
+    plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+    plt.legend(loc='lower right')
+    plt.title('Training and Validation Accuracy')
+    plt.xlabel("Aantal epochs")
+    plt.ylabel("Accuracy")
+
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs_range, loss, label='Training Loss')
+    plt.plot(epochs_range, val_loss, label='Validation Loss')
+    plt.legend(loc='upper right')
+    plt.title('Training and Validation Loss')
+    plt.xlabel("Aantal epochs")
+    plt.ylabel("Accuracy")
+    plt.show()
+
+
 if __name__ == '__main__':
     train, validatie, class_names = datasets()
     visualisatie_images(train, class_names)
     train, validatie = autotune(train, validatie)
-    t_acc, v_acc, t_loss, v_loss, epochs_range, data_augmentation = \
+    t_acc, v_acc, t_loss, v_loss, epochs_range, data_aug = \
         model_voor_augmentatie(train, validatie, class_names)
     figure_model(t_acc, v_acc, t_loss, v_loss, epochs_range)
+    augmentation_figure(train, data_aug)
+    history, epochs = model_aug(data_aug, class_names, train, validatie)
+    figure_aug(history, epochs)
