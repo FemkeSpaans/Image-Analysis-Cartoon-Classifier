3de079d40fdf07e7d5fdf8e3b2f6b5f2327b7f1d
FSpaa <f.spaans@hotmail.nl>
2023-06-09 11:44:55 +0200

    Documentation amended

--- converter jpg to png.py
+++ converter jpg to png.py
@@ -1,57 +1,60 @@
+"""Converts image files saved in a jpg format to a png format.
+"""
+
 from PIL import Image
 import os
 
 
-def filedirectory(filedir):
-    """
+def file_directory(file_dir):
+    """ Creates a list containing the images files from a directory.
 
-    :param filedir:
-    :return:
+    :param file_dir: contains 5 lists of the directories with the image files.
+    :return file_list: list containing the directories and the respective
+    images.
     """
-    filelist = []
-    for filename in os.listdir(filedir):
-        f = os.path.join(filedir, filename)
-        filelist.append(f)
-    return filelist
+    file_list = []
+    for file_name in os.listdir(file_dir):
+        f = os.path.join(file_dir, file_name)
+        file_list.append(f)
+    return file_list
 
 
-def convert_image(filedirectory):
-    """
+def convert_image(file_directory):
+    """Replaces the .jpg format to .png format.
 
-    :param filedirectory:
-    :return:
+    :param file_directory: list containing the paths of the directories.
     """
-    im = Image.open(filedirectory)
-    namereplacement = filedirectory.replace(".jpg", "")
-    im.save(namereplacement + ".png", "PNG")
+    im = Image.open(file_directory)
+    name_replacement = file_directory.replace(".jpg", "")
+    im.save(name_replacement + ".png", "PNG")
 
 
 def main():
-    allfilelists = []
-    fileswithpicturepath = []
-    # Kijkt naar de eerste volgende path (test, train, validatie)
-    filelist = filedirectory("cartoon_backup\\data")
+    all_file_lists = []
+    files_with_picture_path = []
+    # Finds the next directory in the path (test, train, validation).
+    file_list = file_directory("cartoon_backup\\data")
     print("x")
 
-    # Kijkt naar de eerst volgende path (bean, conan, etc.)
-    for i in filelist:
-        filelist = filedirectory(i)
-        allfilelists.append(filelist)
+    # Finds the next directory in the path (bean, conan, etc.).
+    for i in file_list:
+        file_list = file_directory(i)
+        all_file_lists.append(file_list)
     print("xx")
 
-    # Kijkt naar images in de hele path (1.jpg, 2.jpg, 3.jpg, etc...)
-    for i in allfilelists:
+    # Finds the next image in the path (1.jpg, 2.jpg, 3.jpg, etc...).
+    for i in all_file_lists:
         for i2 in i:
-            filewithpicturelist = filedirectory(i2)
-            fileswithpicturepath.append(filewithpicturelist)
+            file_with_picture_list = file_directory(i2)
+            files_with_picture_path.append(file_with_picture_list)
     print("xxx")
 
-    # Pakt de hele path van de images
-    for dir in fileswithpicturepath:
-        for picture in dir:
-            # Zet de images om van jpg naar png
+    # Entire path to the images.
+    for directory in files_with_picture_path:
+        for picture in directory:
+            # Converts the images from .jpg to .png.
             convert_image(picture)
-            # verwijderd de jpg image
+            # Deletes the .jpg image files.
             os.remove(picture)
     print("xxxx")
 
